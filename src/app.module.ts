7ccd4ed683522ca3750c748b50362489014df6f5
Rahul-thakur25 <127794497+Rahul-thakur25@users.noreply.github.com>
2025-04-07 22:15:32 +0530

    Nest setup

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma/prisma.service';
+@Module({
+  imports: [AuthModule,UserModule, BookmarkModule],
+  providers: [PrismaService]
+})
+export class AppModule {}  
